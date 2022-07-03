c40601dd538be6df4d8551f9291c0ec6e992e638
Mohammadmahdi Sefatzadeh <mm.80xefatzadeh@gmail.com>
2022-07-03 15:25:15 +0430

    reformatted files using intellij reformatter

--- src/domain/Course.java
+++ src/domain/Course.java
@@ -1,61 +1,61 @@
 package domain;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Course {
-	private String id;
-	private String name;
-	private int units;
-	
-	List<Course> prerequisites;
+    List<Course> prerequisites;
+    private final String id;
+    private final String name;
+    private final int units;
 
-	public Course(String id, String name, int units) {
-		this.id = id;
-		this.name = name;
-		this.units = units;
-		prerequisites = new ArrayList<Course>();
-	}
-	
-	public void addPre(Course c) {
-		getPrerequisites().add(c);
-	}
+    public Course(String id, String name, int units) {
+        this.id = id;
+        this.name = name;
+        this.units = units;
+        prerequisites = new ArrayList<>();
+    }
 
-	public Course withPre(Course... pres) {
-		prerequisites.addAll(Arrays.asList(pres));
-		return this;
-	}
+    public void addPre(Course c) {
+        getPrerequisites().add(c);
+    }
 
-	public List<Course> getPrerequisites() {
-		return prerequisites;
-	}
+    public Course withPre(Course... pres) {
+        prerequisites.addAll(Arrays.asList(pres));
+        return this;
+    }
 
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(name);
-		sb.append(" {");
-		for (Course pre : getPrerequisites()) {
-			sb.append(pre.getName());
-			sb.append(", ");
-		}
-		sb.append("}");
-		return sb.toString();
-	}
+    public List<Course> getPrerequisites() {
+        return prerequisites;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(" {");
+        for (Course pre : getPrerequisites()) {
+            sb.append(pre.getName());
+            sb.append(", ");
+        }
+        sb.append("}");
+        return sb.toString();
+    }
 
-	public int getUnits() {
-		return units;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public int getUnits() {
+        return units;
+    }
 
-	public boolean equals(Object obj) {
-		Course other = (Course)obj;
-		return id.equals(other.id);
-	}
+    public String getId() {
+        return id;
+    }
+
+    public boolean equals(Object obj) {
+        Course other = (Course) obj;
+        return id.equals(other.id);
+    }
 }
--- src/domain/Student.java
+++ src/domain/Student.java
@@ -1,43 +1,34 @@
 package domain;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Student {
-	private String id;
-	private String name;
-
-	static class CourseSection {
-        CourseSection(Course course, int section) {
-            this.course = course;
-            this.section = section;
-        }
-        Course course;
-	    int section;
+    private final String id;
+    private final String name;
+    private final Map<Term, Map<Course, Double>> transcript;
+    private final List<CourseSection> currentTerm;
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.transcript = new HashMap<>();
+        this.currentTerm = new ArrayList<>();
     }
-	private Map<Term, Map<Course, Double>> transcript;
-	private List<CourseSection> currentTerm;
 
-	public Student(String id, String name) {
-		this.id = id;
-		this.name = name;
-		this.transcript = new HashMap<>();
-		this.currentTerm = new ArrayList<>();
-	}
-	
-	public void takeCourse(Course c, int section) {
-		currentTerm.add(new CourseSection(c, section));
-	}
+    public void takeCourse(Course c, int section) {
+        currentTerm.add(new CourseSection(c, section));
+    }
 
-	public Map<Term, Map<Course, Double>> getTranscript() {
-		return transcript;
-	}
+    public Map<Term, Map<Course, Double>> getTranscript() {
+        return transcript;
+    }
 
-	public void addTranscriptRecord(Course course, Term term, double grade) {
-	    if (!transcript.containsKey(term))
-	        transcript.put(term, new HashMap<>());
-	    transcript.get(term).put(course, grade);
+    public void addTranscriptRecord(Course course, Term term, double grade) {
+        if (!transcript.containsKey(term))
+            transcript.put(term, new HashMap<>());
+        transcript.get(term).put(course, grade);
     }
 
     public List<CourseSection> getCurrentTerm() {
@@ -45,14 +36,23 @@ public class Student {
     }
 
     public String getId() {
-		return id;
-	}
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
-	
-	public String toString() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    static class CourseSection {
+        Course course;
+        int section;
+        CourseSection(Course course, int section) {
+            this.course = course;
+            this.section = section;
+        }
+    }
 }
